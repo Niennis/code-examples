6734775219b5bfe2c6781f83402b9928b338bd59
Estefanía Osses Vera <32283958+Niennis@users.noreply.github.com>
2018-02-24 19:33:54 -0300

    Update access_token.js

--- authentication/nodejs/access_token.js
+++ authentication/nodejs/access_token.js
@@ -1,6 +1,6 @@
 var MP = require ("mercadopago");
 
-var mp = new MP ("CLIENT_ID", "CLIENT_SECRET");
+var mp = new MP ("7272722303979733", "td7ESCGXlb7AckGZwUvACT07TSmy7Bpu");
 
 mp.getAccessToken(function (err, accessToken){
 	console.log (accessToken);
